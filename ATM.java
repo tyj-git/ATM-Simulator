90150750e6f96cee576b46187a59dcf15beedf8d
lsw <lswworker@163.com>
2026-05-30 19:00:25 +0800

    新增查询余额功能，初始余额10000元

--- ATM.java
+++ ATM.java
@@ -6,7 +6,8 @@ import java.awt.event.ActionListener;
 public class ATM {
     private String cardId = "123456";
     private String password = "123456";
-    private JFrame loginFrame;
+    private double balance = 10000;
+    private JFrame loginFrame, mainFrame;
     private JTextField cardField, pwdField;
 
     public void createLoginUI() {
@@ -29,16 +30,31 @@ public class ATM {
         loginFrame.setVisible(true);
     }
 
+    private void createMainUI() {
+        loginFrame.dispose();
+        mainFrame = new JFrame("ATM柜员机系统");
+        mainFrame.setSize(400, 350);
+        mainFrame.setLayout(new GridLayout(5,1,10,10));
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton queryBtn = new JButton("1. 查询余额");
+        JButton exitBtn = new JButton("退出");
+
+        queryBtn.addActionListener(e -> JOptionPane.showMessageDialog(null,"当前余额："+balance+"元"));
+        exitBtn.addActionListener(e->System.exit(0));
+
+        mainFrame.add(queryBtn);
+        mainFrame.add(exitBtn);
+        mainFrame.setVisible(true);
+    }
+
     class LoginListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
             String cid = cardField.getText();
             String pwd = pwdField.getText();
-            if(cid.equals(cardId) && pwd.equals(password)) {
-                JOptionPane.showMessageDialog(null,"登录成功！");
-            } else {
-                JOptionPane.showMessageDialog(null,"卡号或密码错误！");
-            }
+            if(cid.equals(cardId) && pwd.equals(password)) createMainUI();
+            else JOptionPane.showMessageDialog(null,"卡号或密码错误！");
         }
     }
 
